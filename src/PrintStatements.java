c0bbcc05cf6e666e5d4061d29a96127be36a3b25
prachijainsre05-cell <prachijainsre05@gmail.com>
2026-02-17 15:59:14 +0530

    Update PrintStatements.java

--- src/PrintStatements.java
+++ src/PrintStatements.java
@@ -1,7 +1,12 @@
-System.out.println("  ***  "+" "+"  *****");
-System.out.println(" **  **"+" "+" **    ");
-System.out.println("**   **"+" "+"**     ");
-System.out.println("**   **"+" "+"  *****");
-System.out.println("**   **"+" "+"     **");
-System.out.println(" ** ** "+" "+" **  **");
-System.out.println("  ***  "+" "+" ***** ");
+public class OOPSBannerApp{
+    public static void main(String[] args){
+         System.out.println("   ***      ***     *******     ***** ");
+        System.out.println("  **  **    ** **    **    **   **    ");
+        System.out.println(" **    **  **   **    **    **   **    ");
+        System.out.println(" **    **  **   **    *******      *** ");
+        System.out.println(" **    **  **   **    **               **");
+        System.out.println("  **  **    ** **     **               **");
+        System.out.println("   ***      ***       **           ***** ");
+
+    }
+}
