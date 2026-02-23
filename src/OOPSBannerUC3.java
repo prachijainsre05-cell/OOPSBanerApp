8619c11eee845390c7a909771eaf60e7c2c90b93
Prachi Jain <prachijainsre05@gmail.com>
2026-02-23 16:14:43 +0530

    UC3: Implemented String.join() in OOPSBannerUC3

---
+++ src/OOPSBannerUC3.java
@@ -0,0 +1,16 @@
+public class OOPSBannerUC3 {
+
+    public static void main(String[] args) {
+
+        System.out.println(String.join(" ", "     ***  ", "      ***   ", "  ****** ", "       *****  "));
+        System.out.println(String.join(" ", "   **   ** ", "   **   ** ", "  **    ** ", "   **     "));
+        System.out.println(String.join(" ", "  **     ** ", " **     ** ", " **     ** ", " **     "));
+        System.out.println(String.join(" ", "  **     ** ", " **     ** ", " **    ** ", "   **     "));
+        System.out.println(String.join(" ", "  **     ** ", " **     ** ", " ******   ", "     ***     "));
+        System.out.println(String.join(" ", "  **     ** ", " **     ** ", " **  ", "             **    "));
+        System.out.println(String.join(" ", "  **     ** ", " **     ** ", " **       ", "         **    "));
+        System.out.println(String.join(" ", "   **   ** ", "   **   ** ", "  **       ", "        **    "));
+        System.out.println(String.join(" ", "     ***   ", "     ***   ", "  **        ", "  ***** "));
+    }
+}
+
